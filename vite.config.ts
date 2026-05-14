3328a9b63f5aa7b149942bad9b7a8310884f935e
Tim Lai <timlaicodes@gmail.com>
2026-05-13 21:44:57 -0400

    Added glsl to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import glsl from "vite-plugin-glsl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react(),
+    basicSsl(),
+    glsl({
+      include: [
+        // Glob pattern, or array of glob patterns to import
+        "**/*.glsl",
+        "**/*.wgsl",
+        "**/*.vert",
+        "**/*.frag",
+        "**/*.vs",
+        "**/*.fs",
+      ],
+      exclude: undefined, // Glob pattern, or array of glob patterns to ignore
+      warnDuplicatedImports: true, // Warn if the same chunk was imported multiple times
+      removeDuplicatedImports: false, // Automatically remove an already imported chunk
+      defaultExtension: "glsl", // Shader suffix when no extension is specified
+      minify: true, // Minify/optimize output shader code
+      watch: true, // Recompile shader on change
+      root: "/", // Directory for root imports
+    }),
+  ],
 });
