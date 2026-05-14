b4dded18edf543fd0716c0beaca778bacd0f9dce
Tim Lai <timlaicodes@gmail.com>
2026-05-13 21:48:09 -0400

    Added ViteImageOptimizer to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import glsl from "vite-plugin-glsl";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -25,6 +26,68 @@ export default defineConfig({
       minify: true, // Minify/optimize output shader code
       watch: true, // Recompile shader on change
       root: "/", // Directory for root imports
+    }),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+      exclude: undefined,
+      include: undefined,
+      includePublic: true,
+      logStats: true,
+      ansiColors: true,
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: "preset-default",
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                cleanupIds: {
+                  minify: false,
+                  remove: false,
+                },
+                convertPathData: false,
+              },
+            },
+          },
+          "sortAttrs",
+          {
+            name: "addAttributesToSVGElement",
+            params: {
+              attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+            },
+          },
+        ],
+      },
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 100,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 100,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 100,
+      },
+      tiff: {
+        // https://sharp.pixelplumbing.com/api-output#tiff
+        quality: 100,
+      },
+      // gif does not support lossless compression
+      // https://sharp.pixelplumbing.com/api-output#gif
+      gif: {},
+      webp: {
+        // https://sharp.pixelplumbing.com/api-output#webp
+        lossless: true,
+      },
+      avif: {
+        // https://sharp.pixelplumbing.com/api-output#avif
+        lossless: true,
+      },
+      cache: false,
+      cacheLocation: undefined,
     }),
   ],
 });
