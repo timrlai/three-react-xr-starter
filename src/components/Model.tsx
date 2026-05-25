859b04a6782502bb629c54f0859c6eb7583357e8
Tim Lai <timlaicodes@gmail.com>
2026-05-25 19:57:56 -0400

    Created Model component

---
+++ src/components/Model.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import type { Mesh } from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { useLoader } from "@react-three/fiber";
+
+type ModelProps = {
+  position?: [number, number, number];
+  rotation?: [number, number, number];
+  scale?: number;
+};
+
+export default function Model({
+  position = [-6, -2, 1],
+  rotation = [0, 1.6, 0],
+  scale = 1,
+}: ModelProps) {
+  const gltf = useLoader(GLTFLoader, "/models/table.gltf");
+  const clone = gltf.scene.clone(true);
+
+  useEffect(() => {
+    if (!clone) return;
+
+    clone.traverse((object) => {
+      if ((object as Mesh).isMesh) {
+        const mesh = object as Mesh;
+        mesh.castShadow = true;
+        mesh.receiveShadow = true;
+      }
+    });
+  }, [clone]);
+
+  return (
+    <primitive
+      object={clone}
+      position={position}
+      rotation={rotation}
+      scale={scale}
+    />
+  );
+}
