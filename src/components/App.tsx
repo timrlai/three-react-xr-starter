2653c1172fd6063273120579d830769c556bf45a
Tim Lai <timlaicodes@gmail.com>
2026-05-25 19:59:26 -0400

    Updated App with Model and OrbitControls

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import { XR, createXRStore } from "@react-three/xr";
 import { Box3 } from "three";
 import Intersectable from "./Intersectable";
 import Draggable from "./Draggable";
+import Model from "./Model";
 
 const store = createXRStore();
 
@@ -24,22 +26,27 @@ export default function App() {
       </nav>
       <Canvas>
         <XR store={store}>
-          <Intersectable
-            position={[-2, 1, -1]}
-            box={box}
-            onIntersect={() => setRed(true)}
-          >
-            <mesh>
-              <boxGeometry />
-              <meshBasicMaterial color={red ? "red" : "blue"} />
-            </mesh>
-          </Intersectable>
-          <Draggable position={[2, 1, -1]} onDragged={(box) => setBox(box)}>
-            <mesh>
-              <boxGeometry />
-              <meshBasicMaterial color="red" />
-            </mesh>
-          </Draggable>
+          <Suspense fallback={null}>
+            <Intersectable
+              position={[-2, 1, -1]}
+              box={box}
+              onIntersect={() => setRed(true)}
+            >
+              <mesh>
+                <boxGeometry />
+                <meshBasicMaterial color={red ? "red" : "blue"} />
+              </mesh>
+            </Intersectable>
+            <Draggable position={[2, 1, -1]} onDragged={(box) => setBox(box)}>
+              <mesh>
+                <boxGeometry />
+                <meshBasicMaterial color="red" />
+              </mesh>
+            </Draggable>
+            <Model />
+
+            <OrbitControls />
+          </Suspense>
         </XR>
       </Canvas>
     </main>
